59937c8081b6808e7b5d44d957cf0480e9008f99
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-06-13 20:15:01 +1100

    finish

--- task1.cpp
+++ task1.cpp
@@ -6,12 +6,17 @@ struct statement {
 
 	std::string name = "Unknow";
 	std::string surname = "Unknow";
-	std::string date_of_issue = "00:00:00";
+	std::string date_of_issue = "DD.MM.YYYY";
 	int sum = 0;
 
 };
 
 void recording_data_in_a_register(statement& person) {
+
+	int validation_day;
+	int validation_day_month;
+	int validation_day_year;
+
 
 	std::cout << "Enter name: ";
 	std::cin >> person.name;
@@ -19,44 +24,83 @@ void recording_data_in_a_register(statement& person) {
 	std::cout << "Enter surname: ";
 	std::cin >> person.surname;
 	
-
-	std::cout << "Enter date_of_issue: ";
+	std::cout << "Enter the date of issue of funds in the format DD.MM.YYYY.: ";
 	std::cin >> person.date_of_issue;
+
+	validation_day = std::stoi(person.date_of_issue.substr(0, 2));
+	validation_day_month = std::stoi(person.date_of_issue.substr(3, 2));
+	validation_day_year = std::stoi(person.date_of_issue.substr(6, 4));
+
+	while ((validation_day < 1 || validation_day > 31) || (validation_day_month < 1 || validation_day_month > 12) || (validation_day_year < 2000 || validation_day_year > 2023)) {
+
+		std::cout << "Incorrect date!\n";
+		std::cout << "Enter the date of issue of funds in the format DD.MM.YYYY.: \n";
+		std::cin >> person.date_of_issue;
+
+		validation_day = std::stoi(person.date_of_issue.substr(0, 2));
+		validation_day_month = std::stoi(person.date_of_issue.substr(3, 2));
+		validation_day_year = std::stoi(person.date_of_issue.substr(6, 4));
+
+	}
 
 	std::cout << "Enter sum: ";
 	std::cin >> person.sum;
 
+
 	std::ofstream out_cash_payment_statement("statement.txt", std::ios::app);
 
-	out_cash_payment_statement << person.name << " " << person.surname << " " << person.date_of_issue << " " << person.sum << std::endl;
+	if (out_cash_payment_statement.is_open()) {
 
-	out_cash_payment_statement.close();
+		out_cash_payment_statement << person.name << " " << person.surname << " " << person.date_of_issue << " " << person.sum << std::endl;
 
+		out_cash_payment_statement.close();
+
+	}
+	else {
+
+		std::cout << "WARNING!\n";
+		std::cout << "ERROR!!!\n";
+		std::cout << "File is not open\n";
+
+	}
+	
 }
 
 void reading_data_from_the_registry(statement& person) {
 		
 	std::ifstream in_cash_payment_statement("statement.txt");
 	
-	int count = 0;
-	while (!in_cash_payment_statement.eof()) {			
-		
-		person.name = "";
+	if (in_cash_payment_statement.is_open()) {
 
-		in_cash_payment_statement >> person.name >> person.surname >> person.date_of_issue >> person.sum;		
+		int count = 0;
+		while (!in_cash_payment_statement.eof()) {
 
-		if (person.name == "") {
+			person.name = "";
 
-			break;
+			in_cash_payment_statement >> person.name >> person.surname >> person.date_of_issue >> person.sum;
 
+			if (person.name == "") {
+
+				break;
+
+			}
+
+			std::cout << person.name << " " << person.surname << " " << person.date_of_issue << " " << person.sum << std::endl;
+
+			++count;
 		}
 
-		std::cout << person.name << " " << person.surname << " " << person.date_of_issue << " " << person.sum << std::endl;
+		in_cash_payment_statement.close();
 
-		++count;
+	}
+	else {
+
+		std::cout << "WARNING!\n";
+		std::cout << "ERROR!!!\n";
+		std::cout << "File is not open\n";
+
 	}
 	
-	in_cash_payment_statement.close();
 }
 
 int main() {
